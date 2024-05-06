6773f9f4711c736d5f958fa2c73456bca974ef01
manemael <mael.mane@fractale.vision>
2024-05-06 15:34:31 -0400

    add cryptocurrencies section

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -8,7 +8,7 @@ import {Cryptocurrencies, News} from '../components';
 const {Title} = Typography;
 
 const Homepage = () => {
-    const {data, isFetching} = useGetCryptosQuery();
+    const {data, isFetching} = useGetCryptosQuery(10);
     const globalStats = data?.data?.stats;
 
     console.log(data)
@@ -25,7 +25,7 @@ const Homepage = () => {
         </Row>
         <div className='home-heading-container'>
             <Title level={2} className='home-title'>Top 10 Cryptocurrencies in the world</Title>
-            <Title level={3} className='show-more'><Link to="/cryptocurrencices">Show more</Link></Title>
+            <Title level={3} className='show-more'><Link to="/cryptocurrencies">Show more</Link></Title>
         </div>
         <Cryptocurrencies simplified/>
         <div className='home-heading-container'>
